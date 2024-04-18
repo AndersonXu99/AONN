Fixed data type to avoid overflow issue.

EIT/EIT Analysis Multibeam 5x5 Image with File Renaming.py
@@ -10,7 +10,7 @@ import time
 start_time = time.time()
 
 ### ---------------------------------------------------------------------------------------------------------------- ###
-### AONN                                                                                                             ###
+### AONN - EIT Analysis                                                                                              ###
 ### Author: Anderson Xu                                                                                              ###
 ### ---------------------------------------------------------------------------------------------------------------- ###
 
@@ -23,8 +23,8 @@ start_time = time.time()
 # For example: C:\Users\zxq220007\Box\Quantum Optics Lab\TeTON OANN Testbed\Data 2024\Apr 11 2024\138C\5X5 Trans5 50mm cell 138C 290MHz 3037MHz
 # No need to double lashes in the file path
 # Important: Keep the r in front of the quotation mark
-data_folder_path = r"C:\Users\zxq220007\Box\Quantum Optics Lab\TeTON OANN Testbed\Data 2024\Apr 16 2024\5X5 Trans5 50mm cell 130C 290MHz 3037MHz"
-output_file_name = "Results - New"
+data_folder_path = r"C:\Users\zxq220007\Box\Quantum Optics Lab\TeTON OANN Testbed\Data 2024\Apr 16 2024\5X5 Trans7 50mm cell 130C 290MHz 3037MHz"
+output_file_name = "Results 1"
 
 # path to the csv that maps the 0-20 values to actual powers of the beam
 csv_file_path = r'C:\Users\zxq220007\Box\Quantum Optics Lab\TeTON OANN Testbed\Data 2024\Apr 16 2024\5X5 Mod Depth to Power updated.csv'
@@ -40,7 +40,7 @@ def calculate_EIT(image1, roi1):
     roi1_gray = image1[roi1[0]:roi1[1], roi1[2]:roi1[3]]
 
     # Calculate the sum of grayscale intensity within the first ROI
-    intensity_EIT = np.sum(roi1_gray)
+    intensity_EIT = np.sum(roi1_gray, dtype=float)
 
     return intensity_EIT
 
@@ -49,7 +49,7 @@ def calculate_background(image2, roi2):
     roi2_gray = image2[roi2[0]:roi2[1], roi2[2]:roi2[3]]
 
     # Calculate the sum of grayscale intensity within the first ROI
-    intensity_background = np.sum(roi2_gray)
+    intensity_background = np.sum(roi2_gray, dtype = float)
 
     return intensity_background
 
@@ -113,6 +113,8 @@ BG_main_folder = os.path.join(data_folder_path, 'BG')  # Directory for the secon
 roi_image_dir = os.path.join(data_folder_path, 'ROI.tif')  # ROI image path
 main_output_folder = os.path.join(data_folder_path, output_file_name)
 os.makedirs(main_output_folder, exist_ok=True)  # Create a main output folder
+
+test_run_name = os.path.basename(data_folder_path)
 
 # reading the reference ROI image
 ROI_image = cv2.imread(roi_image_dir, cv2.IMREAD_COLOR)
@@ -264,7 +266,7 @@ for beam_index, (roi1, roi2) in enumerate(zip(roi_coords, roi_coords)):
 
 # Plotting the overall plot
 fig, axs = plt.subplots(5, 5, figsize=(12, 8))  # Create a 5x5 grid of subplots
-
+plt.title(test_run_name)
 # Iterate through each beam and plot its intensity difference curve in the corresponding subplot
 for beam_index in range(len(intensity_difference_values)):
     row_index = beam_index // 5  # Calculate the row index in the grid