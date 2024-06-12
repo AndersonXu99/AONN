0c042a50cc276808d6f7195752b7844fd1ac8522
zxq220007 <zxq220007@utdallas.edu>
2024-06-11 20:20:33 -0500

    Refactored SLM control code into a reusable class.
    - Encapsulated the SLM control code into a new class named SLMController.
    - This class can now be imported and used in different programs, improving code reusability and organization.

--- SLM Phase Pattern/SLM_Control.py
+++ SLM Phase Pattern/SLM_Control.py
@@ -7,6 +7,7 @@
 #                                                                    #
 # You may use this file under the terms and conditions of the        #
 # "HOLOEYE SLM Display SDK Standard License v1.0" license agreement. #
+# This code modified by Anderson Xu                                  #
 #                                                                    #
 #--------------------------------------------------------------------#
 
@@ -15,52 +16,64 @@ from holoeye import slmdisplaysdk
 from showSLMPreview import showSLMPreview
 import random
 
-# Open the SLM window:
-slm = slmdisplaysdk.SLMInstance()
-# Check if the library implements the required version
-if not slm.requiresVersion(3):
-    exit(1)
+class SLMControler: 
+    def __init__(self):
+        # Open the SLM window:
+        self.slm = slmdisplaysdk.SLMInstance()
+        # Check if the library implements the required version
+        if not self.slm.requiresVersion(3):
+            exit(1)
 
-error = slm.open()
-assert error == slmdisplaysdk.ErrorCode.NoError, slm.errorString(error)
+        error = self.slm.open()
+        assert error == slmdisplaysdk.ErrorCode.NoError, self.slm.errorString(error)
 
-# Open the SLM preview window in "Fit" mode:
-# Please adapt the file showSLMPreview.py if preview window
-# is not at the right position or even not visible.
+        # Open the SLM preview window in "Fit" mode:
+        # Please adapt the file showSLMPreview.py if preview window
+        # is not at the right position or even not visible.
 
-showSLMPreview(slm, scale=0.0)
+        showSLMPreview(self.slm, scale=0.0)
 
-# Calculate e.g. a vertical blazed grating:
-# this was used to create a blazed grating
-# blazePeriod = 77
+        # Calculate e.g. a vertical blazed grating:
+        # this was used to create a blazed grating
+        # self.blazePeriod = 77
 
-# Reserve memory for the data:
-dataWidth  = slm.width_px
-dataHeight = slm.height_px
-data = slmdisplaysdk.createFieldSingle(dataWidth, dataHeight)
-# print("dataWidth = " + str(dataWidth))
-# print("dataHeight = " + str(dataHeight))
+        # Reserve memory for the data:
+        self.dataWidth  = self.slm.width_px
+        self.dataHeight = self.slm.height_px
 
-# creating a random phase pattern
-for y in range(dataHeight):
-    row = data[y]
-
-    for x in range(dataWidth):
-        row[x] = random.random() 
-
-# Displaying the data on the SLM using the .showData() method
-error = slm.showData(data)
-assert error == slmdisplaysdk.ErrorCode.NoError, slm.errorString(error)
+    def display_data (self, data):
+        # check if the data has the correct shape
+        assert data.shape == (self.dataHeight, self.dataWidth), "Data shape does not match SLM shape"
+        # Displaying the data on the SLM using the .showData() method
+        error = self.slm.showData(data)
+        assert error == slmdisplaysdk.ErrorCode.NoError, self.slm.errorString(error)
 
 # If your IDE terminates the python interpreter process after the script is finished, the SLM content
 # will be lost as soon as the script finishes.
 
-# You may insert further code here.
+    def close(self):
+        # Wait until the SLM process is closed:
+        print("Waiting for SDK process to close. Please close the tray icon to continue ...")
+        error = self.slm.utilsWaitUntilClosed()
+        assert error == slmdisplaysdk.ErrorCode.NoError, self.slm.errorString(error)
 
-# Wait until the SLM process is closed:
-print("Waiting for SDK process to close. Please close the tray icon to continue ...")
-error = slm.utilsWaitUntilClosed()
-assert error == slmdisplaysdk.ErrorCode.NoError, slm.errorString(error)
+        # Unloading the SDK may or may not be required depending on your IDE:
+        self.slm = None
 
-# Unloading the SDK may or may not be required depending on your IDE:
-slm = None
+
+# Use Case
+slm = SLMControler()
+
+data = slmdisplaysdk.createFieldSingle(slm.dataWidth, slm.dataHeight)
+# print("dataWidth = " + str(dataWidth))
+# print("dataHeight = " + str(dataHeight))
+
+# creating a random phase pattern
+for y in range(slm.dataHeight):
+    row = data[y]
+
+    for x in range(slm.dataWidth):
+        row[x] = random.random() 
+
+slm.display_data(data)
+slm.close()
